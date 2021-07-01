92c571db7b522cf5b8daeb8384224fcdb1c76367
Josef Erben <josef@oxidizing.io>
2021-07-01 13:42:41 +0200

    Prepare more user stories for implementation

--- pool/ddd.ml
+++ pool/ddd.ml
@@ -1,4 +1,4 @@
-(* Infrastructure *)
+(* Framework Infrastructure *)
 
 let todo = failwith "todo"
 
@@ -7,13 +7,35 @@ module User = struct
 end
 
 type timestamp = unit
-type data = (string * string list) list
 type 'a io = 'a
 
-(* Domain *)
+(* Application Infrastructure *)
 
-type event = SignedUp of User.t
+type setting =
+  | Languages
+  | Email_suffixes
+
+type setting_value = setting * string
+
+type event =
+  | SignedUp of User.t
+  | SettingsChanged of setting list
+
 type command_result = (event list, string) Result.t
+
+(* https://gitlab.uzh.ch/econ/study-coordination/pool/-/issues/5 *)
+
+module ChangeSettings : sig
+  type t
+
+  val handle : t -> command_result
+end = struct
+  type t = setting list
+
+  let[@warning "-27"] handle command = todo
+end
+
+(* Domain *)
 
 (* https://gitlab.uzh.ch/econ/study-coordination/pool/-/issues/9 *)
 
@@ -31,8 +53,6 @@ module SignUp : sig
     -> ?password_policy:(string -> (unit, string) Result.t)
     -> User.t list
     -> command_result
-
-  val of_data : data -> command_result io
 end = struct
   type t =
     { email : string
@@ -43,14 +63,72 @@ end = struct
     ; terms_accepted_at : timestamp
     }
 
-  let[@warning "-27"] handle
-      command
-      ?(allowed_email_suffixes = [])
-      ?(password_policy = todo)
-      similar_users
-    =
-    todo
-  ;;
+  let handle _ ?allowed_email_suffixes:_ ?password_policy:_ _ = todo
+end
 
-  let[@warning "-27"] of_data data = todo
+(* https://gitlab.uzh.ch/econ/study-coordination/pool/-/issues/3 *)
+
+type client =
+  { id : string
+  ; title : string
+  ; description : string
+  ; url : string
+  ; database : string
+  ; styles : string
+  ; icon : string
+  ; logos : string
+  ; partner_logos : string
+  ; disabled : bool
+  }
+
+module AddClient : sig
+  type t
+
+  val handle : t -> command_result
+end = struct
+  type t
+
+  let handle = todo
+end
+
+module EditClient : sig
+  type t
+
+  val handle : t -> client -> command_result
+end = struct
+  type t
+
+  let handle _ _ = todo
+end
+
+type operator = unit
+
+module AddOperator : sig
+  type t
+
+  val handle : t -> client -> command_result
+end = struct
+  type t
+
+  let handle _ _ = todo
+end
+
+module ActiveOperator : sig
+  type t
+
+  val handle : t -> operator -> command_result
+end = struct
+  type t
+
+  let handle _ _ = todo
+end
+
+module DeactivateOperator : sig
+  type t
+
+  val handle : t -> operator -> command_result
+end = struct
+  type t
+
+  let handle _ _ = todo
 end
