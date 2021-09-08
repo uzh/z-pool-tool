dcdb66b4040a723b1763e740e4f1343d68eb7abc
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2021-09-08 14:54:54 +0200

    fix participant events

--- pool/app/participant/event.ml
+++ pool/app/participant/event.ml
@@ -20,8 +20,6 @@ type update =
 
 type event =
   | Created of create
-  | EmailUpdated of t * Email.unverified Email.t
-  | EmailVerified of Email.unverified Email.t
   | DetailsUpdated of t * update
   | PasswordUpdated of t * Password.t * PasswordConfirmed.t
   | Disabled of t
@@ -31,30 +29,19 @@ let handle_event : event -> unit Lwt.t =
   let open Lwt.Syntax in
   function
   | Created participant ->
-    let email_address = participant.email |> Email.Address.show in
-    let* token =
-      Service.Token.create
-        [ "email", email_address
-        ; "firstname", participant.firstname
-        ; "lastname", participant.lastname
-        ]
-    in
-    let email = Email.Unverified { address = email_address; token } in
     let* user =
       Service.User.create_user
         ~name:participant.firstname
         ~given_name:participant.lastname
         ~password:participant.password
-        (Email.show email)
+      @@ Email.Address.show participant.email
     in
     let* () = Permission.assign user (Role.participant user.id) in
     Repo.insert participant
-  | EmailVerified email -> Common.Event.Email.(handle_event @@ Verified email)
   | DetailsUpdated (params, person) -> Repo.update person params
   | PasswordUpdated (person, password, confirmed) ->
     let* _ = Repo.set_password person password confirmed in
     Lwt.return_unit
-  | EmailUpdated (_, _) -> Utils.todo ()
   | Disabled _ -> Utils.todo ()
   | Verified _ -> Utils.todo ()
 ;;
@@ -62,13 +49,10 @@ let handle_event : event -> unit Lwt.t =
 let equal_event (one : event) (two : event) : bool =
   match one, two with
   | Created m, Created p -> equal_create m p
-  | EmailVerified m, EmailVerified p -> Email.equal m p
   | DetailsUpdated (p1, one), DetailsUpdated (p2, two) ->
     equal p1 p2 && equal_update one two
   | PasswordUpdated (p1, one, _), PasswordUpdated (p2, two, _) ->
     equal p1 p2 && Password.equal one two
-  | EmailUpdated (p1, one), EmailUpdated (p2, two) ->
-    equal p1 p2 && Email.equal one two
   | Disabled p1, Disabled p2 -> equal p1 p2
   | Verified p1, Verified p2 -> equal p1 p2
   | _ -> false
@@ -78,15 +62,11 @@ let pp_event formatter (event : event) : unit =
   let person_pp = pp formatter in
   match event with
   | Created m -> pp_create formatter m
-  | EmailVerified m -> Email.pp formatter m
   | DetailsUpdated (p1, updated) ->
     let () = person_pp p1 in
     pp_update formatter updated
   | PasswordUpdated (p1, _, _) ->
     let () = person_pp p1 in
     Password.pp formatter "******"
-  | EmailUpdated (p1, updated) ->
-    let () = person_pp p1 in
-    Email.pp formatter updated
   | Disabled p1 | Verified p1 -> person_pp p1
 ;;
