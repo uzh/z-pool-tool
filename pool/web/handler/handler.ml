16e590cc7005194e58f713fb6ec80bc764ee59d4
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2021-10-08 15:52:12 +0200

    fix all lwt syntax usage (new %lwt)

--- pool/web/handler/handler.ml
+++ pool/web/handler/handler.ml
@@ -1,5 +1,6 @@
 (* This is an example HTTP handler to showcase the usage of CQRS command
    handlers. *)
+
 module Public = Public
 module Participant = Handler_participant
 module Root = Root_handlers
