3ae45fad005f600574a9b47be9c5f759ab6fc3cc
Timo Huber <timo.huber@econ.uzh.ch>
2021-10-27 11:28:51 +0200

    use list.iter instead of list.map

--- pool/database/seed/seed_user.ml
+++ pool/database/seed/seed_user.ml
@@ -9,28 +9,29 @@ let admins db_pool () =
     Sys.getenv_opt "POOL_ADMIN_DEFAULT_PASSWORD"
     |> Option.value ~default:"admin"
   in
-  let result =
-    Lwt_list.map_s (fun (given_name, name, email, role) ->
-        let%lwt user = Service.User.find_by_email_opt ~ctx email in
-        match user with
-        | None ->
-          let%lwt user =
-            Service.User.create_admin ~ctx ~name ~given_name ~password email
-          in
-          let person = Admin.create_person user in
-          (match role with
+  Lwt_list.iter_s
+    (fun (given_name, name, email, role) ->
+      let%lwt user = Service.User.find_by_email_opt ~ctx email in
+      match user with
+      | None ->
+        let%lwt user =
+          Service.User.create_admin ~ctx ~name ~given_name ~password email
+        in
+        let person = Admin.create_person user in
+        let%lwt _ =
+          match role with
           | `Assistant -> Admin.insert db_pool (Admin.Assistant person)
           | `Experimenter -> Admin.insert db_pool (Admin.Experimenter person)
           | `Recruiter -> Admin.insert db_pool (Admin.Recruiter person)
           | `LocationManager ->
             Admin.insert db_pool (Admin.LocationManager person)
-          | `Operator -> Admin.insert db_pool (Admin.Operator person))
-        | Some _ ->
-          Logs.debug (fun m -> m "%s" "Admin user already exists");
-          Lwt.return_unit |> Lwt_result.ok)
-  in
-  let%lwt _ = result data in
-  Lwt.return_unit
+          | `Operator -> Admin.insert db_pool (Admin.Operator person)
+        in
+        Lwt.return_unit
+      | Some _ ->
+        Logs.debug (fun m -> m "%s" "Admin user already exists");
+        Lwt.return_unit)
+    data
 ;;
 
 let participants db_pool () =
