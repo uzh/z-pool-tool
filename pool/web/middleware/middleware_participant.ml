38317287071be7a987055a37d64984aecc0ee837
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2021-11-03 17:27:53 +0100

    fix confirmed participant middleware

--- pool/web/middleware/middleware_participant.ml
+++ pool/web/middleware/middleware_participant.ml
@@ -2,7 +2,7 @@ module Message = Http_utils.Message
 
 let confirmed () =
   let filter handler req =
-    let%lwt is_confirmed =
+    let%lwt confirmed_participant =
       let open Lwt_result.Syntax in
       let* user_id =
         req
@@ -15,12 +15,15 @@ let confirmed () =
         Middleware_tenant.tenant_db_of_request req
         |> Lwt_result.map_err (fun msg -> `TenantNotFound, msg)
       in
-      Lwt_result.map
-        (fun participant -> participant.Participant.user.Sihl_user.confirmed)
+      Lwt_result.bind_result
         (Participant.find pool user_id
         |> Lwt_result.map_err (fun msg -> `ParticipantError, msg))
+        (fun participant ->
+          if participant.Participant.user.Sihl_user.confirmed
+          then Ok participant
+          else Error (`ParticipantUnconfirmed, "Participant isn't confirmed!"))
     in
-    match is_confirmed with
+    match confirmed_participant with
     | Ok _ -> handler req
     | Error (label, _) ->
       (match label with
