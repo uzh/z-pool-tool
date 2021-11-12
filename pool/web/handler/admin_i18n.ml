8cb23683c0b423b55c4358e26b809b68290e9fb2
Timo Huber <timo.huber@econ.uzh.ch>
2021-11-12 09:19:12 +0100

    sort translations

--- pool/web/handler/admin_i18n.ml
+++ pool/web/handler/admin_i18n.ml
@@ -6,12 +6,29 @@ let index req =
   let error_path = "/" in
   let result =
     let open Lwt_result.Syntax in
+    let sort translations =
+      let hash = Hashtbl.create ~random:true (CCList.length translations) in
+      let _ =
+        CCList.map
+          (fun (translation : I18n.t) ->
+            let key = translation.I18n.key |> I18n.Key.value in
+            match Hashtbl.find_opt hash key with
+            | None -> Hashtbl.add hash key [ translation ]
+            | Some lst -> Hashtbl.replace hash key (CCList.cons translation lst))
+          translations
+      in
+      hash
+      |> Hashtbl.to_seq
+      |> CCList.of_seq
+      |> CCList.sort (fun (k1, _) (k2, _) -> CCString.compare k1 k2)
+      |> Lwt.return
+    in
     let message =
       Sihl.Web.Flash.find_alert req |> CCFun.flip Option.bind Message.of_string
     in
     let csrf = Sihl.Web.Csrf.find req |> Option.get in
     let* tenant_db = Middleware.Tenant.tenant_db_of_request req in
-    let%lwt translation_list = I18n.find_all tenant_db () in
+    let%lwt translation_list = I18n.find_all tenant_db () >|> sort in
     Page.Admin.I18n.list csrf translation_list message ()
     |> Sihl.Web.Response.of_html
     |> Lwt.return_ok
--- pool/web/view/page/page_admin_i18n.ml
+++ pool/web/view/page/page_admin_i18n.ml
@@ -1,5 +1,4 @@
 open Tyxml.Html
-(* module HttpUtils = Http_utils *)
 
 let input_element = Component.input_element
 
@@ -7,26 +6,38 @@ let list csrf translation_list message () =
   let open I18n in
   let build_translations_row translation_list =
     CCList.map
-      (fun (translation : I18n.t) ->
-        let action =
-          Sihl.Web.externalize_path
-            (Format.asprintf
-               "/admin/i18n/%s"
-               (translation.id |> Pool_common.Id.value))
+      (fun (key, translations) ->
+        let translations_html =
+          CCList.map
+            (fun translation ->
+              let action =
+                Sihl.Web.externalize_path
+                  (Format.asprintf
+                     "/admin/i18n/%s"
+                     (translation.id |> Pool_common.Id.value))
+              in
+              div
+                [ p [ txt (translation.language |> Pool_common.Language.code) ]
+                ; form
+                    ~a:[ a_action action; a_method `Post ]
+                    [ Component.csrf_element csrf ()
+                    ; input_element
+                        `Text
+                        (Some "content")
+                        (translation.content |> I18n.Content.value)
+                    ; input_element `Submit None "Update"
+                    ]
+                ])
+            translations
         in
         div
-          [ h2 [ txt (translation.key |> I18n.Key.value) ]
-          ; p [ txt (translation.language |> Pool_common.Language.code) ]
-          ; form
-              ~a:[ a_action action; a_method `Post ]
-              [ Component.csrf_element csrf ()
-              ; input_element
-                  `Text
-                  (Some "content")
-                  (translation.content |> I18n.Content.value)
-              ; input_element `Submit None "Update"
+          [ h2
+              [ txt
+                  (key
+                  |> CCString.replace ~which:`All ~sub:"_" ~by:" "
+                  |> CCString.capitalize_ascii)
               ]
-          ; hr ()
+          ; div translations_html
           ])
       translation_list
   in
