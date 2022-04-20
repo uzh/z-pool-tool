ca9db64a780bd92ef71774a855f02cbd93bfac48
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2022-04-20 16:58:12 +0200

    add participation model and  repo

---
+++ pool/app/participation/entity.ml
@@ -0,0 +1,60 @@
+module ShowUp = struct
+  type t = bool [@@deriving eq, show]
+
+  let create m = m
+  let value m = m
+end
+
+module Participated = struct
+  type t = bool [@@deriving eq, show]
+
+  let create m = m
+  let value m = m
+end
+
+module MatchesFilter = struct
+  type t = bool [@@deriving eq, show]
+
+  let create m = m
+  let value m = m
+end
+
+module CanceledAt = struct
+  type t = Ptime.t option [@@deriving eq, show]
+
+  let init = None
+  let create_now () = Some (Ptime_clock.now ())
+  let value m = m
+  let sexp_of_t = Utils.Time.ptime_to_sexp
+end
+
+type t =
+  { id : Pool_common.Id.t
+  ; participant : Participant.t
+  ; show_up : ShowUp.t
+  ; participated : Participated.t
+  ; matches_filter : MatchesFilter.t
+  ; canceled_at : CanceledAt.t
+  ; created_at : Pool_common.CreatedAt.t
+  ; updated_at : Pool_common.UpdatedAt.t
+  }
+[@@deriving eq, show]
+
+let create
+    ?(id = Pool_common.Id.create ())
+    ?(show_up = ShowUp.create false)
+    ?(participated = Participated.create false)
+    ?(matches_filter = MatchesFilter.create true)
+    ?(canceled_at = CanceledAt.init)
+    participant
+  =
+  { id
+  ; participant
+  ; show_up
+  ; participated
+  ; matches_filter
+  ; canceled_at
+  ; created_at = Pool_common.CreatedAt.create ()
+  ; updated_at = Pool_common.UpdatedAt.create ()
+  }
+;;
--- pool/app/participation/participation.ml
+++ pool/app/participation/participation.ml
@@ -1,11 +1,3 @@
-type t =
-  { id : Pool_common.Id.t
-  ; participant : Participant.t
-  ; show_up : bool
-  ; participated : bool
-  ; matches_filter : bool
-  ; canceled_at : Ptime.t
-  ; created_at : Pool_common.CreatedAt.t
-  ; updated_at : Pool_common.UpdatedAt.t
-  }
-[@@deriving eq, show]
+include Entity
+
+let find = Repo.find
