b46140cec58c94ccfb5f64e069433888557e3a81
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2022-09-19 16:18:43 +0200

    update custom field entity

--- pool/app/custom_field/entity/entity.ml
+++ pool/app/custom_field/entity/entity.ml
@@ -2,13 +2,11 @@ module Id = struct
   include Pool_common.Id
 end
 
-module Model = struct
-  let print m fmt _ = Format.pp_print_string fmt m
-
-  type _ t =
-    | Contact : Contact.t -> [> `Single ] t [@printer print "contact"]
-    | Experiment : Experiment.t -> [> `Single ] t [@printer print "experiment"]
-    | Session : Session.t -> [> `Single ] t [@printer print "session"]
+module ModelOptions = struct
+  type t =
+    | Contact
+    | Experiment
+    | Session
   [@@deriving eq, show]
 end
 
@@ -40,34 +38,89 @@ module Required = struct
 end
 
 module Metadata = struct
-  type t = bool [@@deriving eq, show]
-end
+  module Admin = struct
+    type t =
+      { view_only : bool
+      ; input_only : bool
+      ; hint : string
+      ; override : bool
+      }
+    [@@deriving eq, show]
+  end
 
-module Admin = struct
-  type t =
-    { view_only : bool
-    ; inpup_only : bool
-    ; hint : string
-    ; override : bool
-    }
-  [@@deriving eq, show]
+  type t = { admin : Admin.t option } [@@deriving eq, show]
+
+  let empty = { admin = None }
 end
 
 module Disabled = struct
   type t = bool [@@deriving eq, show]
 end
 
-module Field = struct
-  type 'a t =
+type field =
+  { id : Id.t
+  ; name : Name.t
+  ; hint : Hint.t
+  ; field_type : FieldType.t
+  ; validation : Validation.t
+  ; required : Required.t
+  ; metadata : Metadata.t
+  ; disabled : Disabled.t
+  }
+
+(* A field should linked to a "category" *)
+type _ t =
+  | Contact : field -> [> `Single ] t
+  | Experiment : field -> [> `Single ] t
+  | Session : field -> [> `Single ] t
+
+module Answer = struct
+  module TextAnswer = struct
+    type t = string [@@deriving eq, show, yojson]
+  end
+
+  module TextAnswerMasked = struct
+    type t = string [@@deriving eq, yojson]
+
+    let show m = CCString.repeat "*" @@ CCString.length m
+
+    let pp (formatter : Format.formatter) (m : t) : unit =
+      Format.fprintf formatter "%s" m
+    ;;
+  end
+
+  type t =
+    | Text of TextAnswer.t
+    | Masked of TextAnswerMasked.t
+  [@@deriving yojson, show, eq]
+
+  let text = function
+    | Text text -> text
+    | Masked text -> TextAnswerMasked.show text
+  ;;
+end
+
+module Populated = struct
+  module AssignedModel = struct
+    let print m fmt _ = Format.pp_print_string fmt m
+
+    type _ t =
+      | Contact : Contact.t -> [> `Single ] t [@printer print "contact"]
+      | Experiment : Experiment.t -> [> `Single ] t
+          [@printer print "experiment"]
+      | Session : Session.t -> [> `Single ] t [@printer print "session"]
+    [@@deriving eq, show]
+  end
+
+  (* TODO: Populated Entity should be linked to a fixed e.g. "Contact.t" *)
+  type _ t =
     { id : Id.t
-    ; model : 'a Model.t
     ; name : Name.t
     ; hint : Hint.t
     ; field_type : FieldType.t
     ; validation : Validation.t
     ; required : Required.t
     ; metadata : Metadata.t
-    ; admin : Admin.t
     ; disabled : Disabled.t
     }
 end
