31606457fbd92c701052899c20d5d2ee9bc14539
Andi Saurer <andreas.saurer@econ.uzh.ch>
2022-12-01 14:48:00 +0100

    use setup script in devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,28 +5,7 @@ sudo chown -R opam: node_modules
 
 opam init -a --shell=zsh
 
-# get newest opam packages
-opam remote remove --all default
-opam remote add default https://opam.ocaml.org
-
-# ensure all system dependencies are installed
-repo=https://github.com/oxidizing/sihl.git
-opam pin add -yn sihl $repo
-opam pin add -yn sihl-cache $repo
-opam pin add -yn sihl-email $repo
-opam pin add -yn sihl-queue $repo
-opam pin add -yn sihl-storage $repo
-opam pin add -yn sihl-token $repo
-opam pin add -yn sihl-user $repo
-opam pin add -yn letters https://github.com/oxidizing/letters.git
-
-# pin custom libraries hosted on the UZH gitlab
-opam pin add -yn canary https://github.com/uzh/canary.git
-opam pin add -yn conformist https://github.com/oxidizing/conformist.git
-opam pin add -ywn guardian https://github.com/uzh/guardian.git
-
-opam pin add -yn pool .
-opam depext -y pool
+/workspace/scripts/setup.sh
 
 # install opam packages used for vscode ocaml platform package
 # e.g. when developing with emax, add also: utop merlin ocamlformat
