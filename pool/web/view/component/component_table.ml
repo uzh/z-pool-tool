da4ef819c22e1e27f087a795a13156c1d069687a
Timo Huber <timo.huber@econ.uzh.ch>
2022-12-01 11:28:50 +0100

    use table class to align items

--- pool/web/view/component/component_table.ml
+++ pool/web/view/component/component_table.ml
@@ -9,20 +9,8 @@ let field_to_txt language =
 
 let fields_to_txt language = CCList.map (field_to_txt language)
 
-let table_head align_last_end elements =
-  CCList.mapi
-    (fun index elm ->
-      let th =
-        match align_last_end && CCList.length elements = index + 1 with
-        | true ->
-          fun elm -> th [ div ~a:[ a_class [ "flexrow"; "justify-end" ] ] elm ]
-        | false -> th ~a:[]
-      in
-      th [ elm ])
-    elements
-  |> tr
-  |> CCList.pure
-  |> thead
+let table_head elements =
+  elements |> CCList.map CCFun.(CCList.pure %> th) |> tr |> CCList.pure |> thead
 ;;
 
 let layout_class = function
@@ -30,11 +18,11 @@ let layout_class = function
   | `Simple -> "simple"
 ;;
 
-let table_classes layout align_top =
+let table_classes layout align_top align_last_end =
   let base = [ "table"; layout_class layout ] in
-  match align_top with
-  | true -> "align-top" :: base
-  | false -> base
+  [ align_top, "align-top"; align_last_end, "align-last-end" ]
+  |> CCList.filter_map (fun (add, str) -> if add then Some str else None)
+  |> CCList.append base
 ;;
 
 let horizontal_table
@@ -44,30 +32,16 @@ let horizontal_table
   ?(align_last_end = false)
   rows
   =
-  let classes = table_classes layout align_top in
-  let thead =
-    CCOption.map (fun thead -> thead |> table_head align_last_end) thead
-  in
+  let classes = table_classes layout align_top align_last_end in
+  let thead = CCOption.map table_head thead in
   table
     ?thead
     ~a:[ a_class classes ]
-    (CCList.map
-       (fun row ->
-         tr
-           (CCList.mapi
-              (fun index cell ->
-                let td =
-                  match align_last_end && CCList.length row = index + 1 with
-                  | true -> td ~a:[ a_class [ "flexrow"; "justify-end" ] ]
-                  | false -> td ~a:[]
-                in
-                td [ cell ])
-              row))
-       rows)
+    (CCList.map (fun row -> tr (CCList.map (fun cell -> td [ cell ]) row)) rows)
 ;;
 
 let vertical_table layout language ?(align_top = false) ?(classnames = []) rows =
-  let classes = table_classes layout align_top in
+  let classes = table_classes layout align_top false in
   table
     ~a:[ a_class (classes @ classnames) ]
     (CCList.map
