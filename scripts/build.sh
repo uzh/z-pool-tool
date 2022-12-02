04a8a953cd98f50e3f984415563789c9639f1d23
Andi Saurer <andreas.saurer@econ.uzh.ch>
2022-12-02 13:23:09 +0100

    run build as single step in container

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+project_dir=/app
+
+# Install system dependencies
+sudo apk add openssl-libs-static mariadb-static sqlite-static zlib-static
+
+# Mark source dir as save
+git config --global --add safe.directory $project_dir
+
+# Setup project
+$project_dir/scripts/setup.sh
+
+# Install project dependencies
+opam install --deps-only --with-test -y .
+
+# Install project dependencies
+opam exec -- dune build --root .
