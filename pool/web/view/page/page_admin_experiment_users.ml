dc5a8ab859d26b919aaa7ca09a6eae90872e00e2
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2022-12-15 00:02:04 +0100

    fix duplicate externalize

--- pool/web/view/page/page_admin_experiment_users.ml
+++ pool/web/view/page/page_admin_experiment_users.ml
@@ -38,9 +38,7 @@ let role_assignment
       [ div [ admin |> Admin.email |> txt ]
       ; form
           ~a:
-            [ a_action
-                (Format.asprintf "%s/%s" (base_url admin) url
-                |> Sihl.Web.externalize_path)
+            [ a_action (Format.asprintf "%s/%s" (base_url admin) url)
             ; a_method `Post
             ]
           [ csrf_element csrf ()
