a5f2a51c59d514e975737f7f008246ae6ab1183b
timohuber <timo.huber@bluemail.ch>
2023-01-16 15:17:54 +0100

    Update pool/app/message_template/entity_guard.ml
    Co-authored-by: Marc <marc.biedermann@econ.uzh.ch>

--- pool/app/message_template/entity_guard.ml
+++ pool/app/message_template/entity_guard.ml
@@ -8,7 +8,7 @@ module Target = struct
         Guard.AuthorizableTarget.make
           (Guard.TargetRoleSet.singleton `MessageTemplate)
           `MessageTemplate
-          (id |> Pool_common.Id.value |> Guard.Uuid.Target.of_string_exn))
+          (id |> Entity.Id.value |> Guard.Uuid.Target.of_string_exn))
       t
     |> Lwt_result.map_error Pool_common.Message.authorization
   ;;
