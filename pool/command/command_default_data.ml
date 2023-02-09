627d20335201afbbc5a6558e03d265551a50361c
Timo Huber <timo.huber@econ.uzh.ch>
2023-02-09 16:39:57 +0100

    include root database

--- pool/command/command_default_data.ml
+++ pool/command/command_default_data.ml
@@ -17,6 +17,10 @@ Example: default_data.insert message_templates
     (function
     | [ "message_templates" ] ->
       let open Utils.Lwt_result.Infix in
+      let root_event =
+        Message_template.(DefaultRestored default_values_root)
+        |> Pool_event.message_template
+      in
       let tenant_event =
         Message_template.(DefaultRestored default_values_tenant)
         |> Pool_event.message_template
@@ -26,6 +30,7 @@ Example: default_data.insert message_templates
         >|> Lwt_list.iter_s (fun pool ->
               (Pool_event.handle_event pool) tenant_event)
       in
+      let%lwt () = Pool_event.handle_event Pool_database.root root_event in
       Lwt.return_some ()
     | _ -> Command_utils.failwith_missmatch help)
 ;;
