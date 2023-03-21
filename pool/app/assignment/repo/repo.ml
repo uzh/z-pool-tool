9af8a6aaf6a6a08f28f0a0f011316b227cb34f43
Timo Huber <timo.huber@econ.uzh.ch>
2023-03-21 13:46:22 +0100

    do not show assignments of canceled experiments on contact experiment page

--- pool/app/assignment/repo/repo.ml
+++ pool/app/assignment/repo/repo.ml
@@ -151,7 +151,10 @@ module Sql = struct
     let joins =
       {sql|
         LEFT JOIN pool_sessions
-        ON pool_assignments.session_uuid = pool_sessions.uuid
+        ON
+          pool_assignments.session_uuid = pool_sessions.uuid
+        AND
+          pool_sessions.canceled_at IS NULL
       |sql}
     in
     {sql|
