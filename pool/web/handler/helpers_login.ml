bb7a26d26291dd94e4370553333560f79402dbc0
Timo Huber <timo.huber@econ.uzh.ch>
2023-04-28 16:17:05 +0200

    track failed login attempts in hashtbl

--- pool/web/handler/helpers_login.ml
+++ pool/web/handler/helpers_login.ml
@@ -1,57 +1,68 @@
+module Label = Pool_database.Label
+
 let src = Logs.Src.create "login helper"
 
 module Cache = struct
-  open CCCache
+  open Hashtbl
 
-  let equal_find_email (l1, e1) (l2, e2) =
-    Pool_database.Label.equal l1 l2 && CCString.equal e1 e2
+  let tbl : (Label.t * string, int * Ptime.t option) t = create 100
+
+  let set key value =
+    Logs.info (fun m -> m "%s" "Set value");
+    replace tbl key value
   ;;
 
-  let cache_find_email
-    : (Pool_database.Label.t * string, int * Ptime.t option) t
-    =
-    replacing ~eq:equal_find_email 2048
-  ;;
-
-  let clear () = clear cache_find_email
+  let remove = remove tbl
+  let find_opt = find_opt tbl
 end
+
+let block_until counter =
+  let minutes =
+    match counter with
+    | _ when counter < 5 -> None
+    | _ when counter < 6 -> Some 1
+    | _ when counter < 7 -> Some 5
+    | _ when counter < 8 -> Some 10
+    | _ when counter < 9 -> Some 30
+    | _ when counter < 10 -> Some 60
+    | _ -> Some (60 * 24)
+  in
+  minutes
+  |> CCOption.map (fun min ->
+       min * 60
+       |> Ptime.Span.of_int_s
+       |> Ptime.add_span (Ptime_clock.now ())
+       |> CCOption.get_exn_or "Invalid time span provided")
+;;
 
 let login database_label email ~password =
   let open Utils.Lwt_result.Infix in
-  let default = 0, None in
-  let increment (counter, blocked) = counter + 1, blocked in
-  let add_to_cache =
-    CCCache.add Cache.cache_find_email (database_label, email)
+  let init = 0, None in
+  let increment counter =
+    let counter = counter + 1 in
+    counter, block_until counter
   in
-  let[@warning "-27"] cb_logger ~in_cache _ _ =
-    CCCache.iter
-      Cache.cache_find_email
-      (fun (database_label, email) (counter, blocked) ->
-      Logs.info (fun m -> m "%s" email);
-      Logs.info (fun m -> m "%i" counter);
-      ())
-  in
-  let init _ = default in
+  let key = database_label, email in
   let handle_login (counter, blocked) =
     let handle_result = function
       | Ok user ->
-        let (_ : bool) = add_to_cache default in
+        let () = Cache.remove key in
         Ok user
       | Error err ->
-        let (_ : bool) = add_to_cache (increment (counter, blocked)) in
+        let () = counter |> increment |> Cache.set key in
         err |> Pool_common.Message.handle_sihl_login_error |> CCResult.fail
     in
-    match blocked with
-    | Some blocked when Ptime.(is_earlier (Ptime_clock.now ()) ~than:blocked) ->
-      Lwt_result.fail Pool_common.Message.LoginEmailBlocked
-    | None | _ ->
+    let login () =
       Service.User.login
         ~ctx:(Pool_database.to_ctx database_label)
         email
         ~password
       ||> handle_result
+    in
+    match blocked with
+    | Some blocked when Ptime.(is_earlier (Ptime_clock.now ()) ~than:blocked) ->
+      Lwt_result.fail Pool_common.Message.LoginEmailBlocked
+    | None | _ -> login ()
   in
-  CCCache.(
-    with_cache ~cb:cb_logger Cache.cache_find_email init (database_label, email))
-  |> handle_login
+  key |> Cache.find_opt |> CCOption.value ~default:init |> handle_login
 ;;
