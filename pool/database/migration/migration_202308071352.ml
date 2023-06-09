108401ef6d9af5a94759414e097e437b4f5d880e
Timo Huber <timo.huber@econ.uzh.ch>
2023-06-09 12:51:09 +0200

    add sign in columns

--- pool/database/migration/migration_202308071352.ml
+++ pool/database/migration/migration_202308071352.ml
@@ -4,10 +4,12 @@ let add_missing_columns_to_contacts =
     {sql|
     ALTER TABLE pool_contacts
       ADD COLUMN secondary_email varchar(255) AFTER email_verified,
-      ADD COLUMN secondary_email_verified timestamp AFTER secondary_email,
+      ADD COLUMN secondary_email_verified timestamp NULL AFTER secondary_email,
       ADD COLUMN sms_deactivated tinyint AFTER phone_number,
       ADD COLUMN admin_notes TEXT DEFAULT NULL AFTER experiment_type_preference_version,
-      ADD COLUMN smtp_bounces_count SMALLINT(3) DEFAULT 0 AFTER profile_update_triggered_at
+      ADD COLUMN smtp_bounces_count SMALLINT(3) DEFAULT 0 AFTER profile_update_triggered_at,
+      ADD COLUMN sign_in_count INTEGER DEFAULT 0 AFTER experiment_type_preference_version,
+      ADD COLUMN last_sign_in_at timestamp NULL AFTER sign_in_count
     |sql}
 ;;
 
