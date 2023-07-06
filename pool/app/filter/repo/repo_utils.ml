e31aa75d28c70b0811add6d0d5b46d132e025a95
Timo Huber <timo.huber@econ.uzh.ch>
2023-07-06 10:46:57 +0200

    add import flag to filter base condition

--- pool/app/filter/repo/repo_utils.ml
+++ pool/app/filter/repo/repo_utils.ml
@@ -19,6 +19,7 @@ let filtered_base_condition ~allow_invited () =
     AND pool_contacts.paused = 0
     AND pool_contacts.disabled = 0
     AND pool_contacts.terms_accepted_at IS NOT NULL
+    AND pool_contacts.import_pending = 0
     |sql}
   in
   let exclude_invited =
