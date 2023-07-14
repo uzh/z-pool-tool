4661db2b81872e6683a445e93b6181c5f7708ee3
Timo Huber <timo.huber@econ.uzh.ch>
2023-07-14 10:44:58 +0200

    call init functions

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,19 @@
 import 'htmx.org'
 import './index.scss'
 import { initDatepicker } from "./flatpickr.js"
-import framework from '../node_modules/@econ/frontend-framework/dist/main'
+import {
+    initConfirmable,
+    initFileInput,
+    initModal,
+    initNotification,
+    initSortable,
+    initDetectFormChanges,
+} from '../node_modules/@econ/frontend-framework/dist/main'
 
 initDatepicker();
+initConfirmable();
+initFileInput();
+initModal();
+initNotification();
+initSortable();
+initDetectFormChanges();
