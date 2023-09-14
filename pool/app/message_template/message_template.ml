5b25fef4aa1b586d9db67bb4c54f11f6bce2f732
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2023-09-14 13:43:38 +0200

    add message template element for start-end-duration of the session

--- pool/app/message_template/message_template.ml
+++ pool/app/message_template/message_template.ml
@@ -155,6 +155,7 @@ let session_params
   in
   [ "sessionId", session_id
   ; "sessionStart", start
+  ; "sessionDateTime", Session.start_end_to_human session
   ; "sessionDuration", duration
   ; "sessionOverview", session_overview
   ]
