b9b396cef4aabc897eeda4821137a87ef092a6c9
Timo Huber <timo.huber@econ.uzh.ch>
2023-09-28 13:26:49 +0200

    update framework

--- pool/web/view/component/component_button_group.ml
+++ pool/web/view/component/component_button_group.ml
@@ -1,11 +1,20 @@
 open Tyxml.Html
 
-let dropdown ?(classnames = []) buttons =
+let dropdown ?(classnames = []) ?(orientation = `Right) buttons =
+  let orientation_cls = function
+    | `Left -> "left"
+    | `Right -> "right"
+  in
   div
     ~a:[ a_class ("button-list" :: classnames) ]
-    [ div [ Component_icon.(to_html EllipsisVertical) ]
-    ; ul
-        ~a:[ a_class [ "dropdown" ] ]
-        (buttons |> CCList.map CCFun.(CCList.return %> li))
+    [ div
+        ~a:[ a_class [ "has-dropdown"; orientation_cls orientation ] ]
+        [ button
+            ~a:[ a_class [ "btn"; "small"; "is-text" ] ]
+            [ Component_icon.(to_html EllipsisVertical) ]
+        ; ul
+            ~a:[ a_class [ "dropdown" ] ]
+            (buttons |> CCList.map CCFun.(CCList.return %> li))
+        ]
     ]
 ;;
