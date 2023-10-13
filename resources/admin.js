4d530923e75ab6819a74bde2ea35e894fa8c3bfa
Timo Huber <timo.huber@econ.uzh.ch>
2023-10-13 12:36:59 +0200

    move dropdown if outside of viewport

--- resources/admin.js
+++ resources/admin.js
@@ -1,16 +1,18 @@
-import { initRoleSearchForm } from "./admin/role-search.js"
-import { initFilterForm } from "./admin/filter.js"
-import { initRichTextEditor } from "./admin/richTextEditor.js"
-import { initCopyClipboard } from "./admin/copyClipboard.js";
+import { initButtonList } from "./admin/buttonList.js"
 import { initCalendar } from './admin/calendar'
+import { initCopyClipboard } from "./admin/copyClipboard.js";
+import { initFilterForm } from "./admin/filter.js"
 import { initPrint } from "./admin/print"
+import { initRichTextEditor } from "./admin/richTextEditor.js"
+import { initRoleSearchForm } from "./admin/role-search.js"
 
-initRoleSearchForm();
-initFilterForm();
-initRichTextEditor();
-initCopyClipboard();
+initButtonList()
 initCalendar();
+initCopyClipboard();
+initFilterForm();
 initPrint();
+initRichTextEditor();
+initRoleSearchForm();
 
 window['pool-tool'] = {
     initRichTextEditor
