33dded67080464e8fcab9a4e6e8560d7df143518
Leandro Ostera <leandro@abstractmachines.dev>
2023-11-16 23:12:56 +0100

    chore: bump to safe mirage-crypto fork of sihl

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,15 +4,15 @@
 set -ex
 
 # get newest opam packages
-opam remote remove --all default
-opam remote add default https://opam.ocaml.org
+# opam remote remove --all default
+# opam remote add default https://opam.ocaml.org
 
 repo_oxi=https://github.com/oxidizing
 repo_uzh=https://github.com/uzh
 repo_sihl=$repo_oxi/sihl.git#e0b304a4ebd3cb2ac0d21e0f6d151b9db301f9eb
 
 # pin Sihl repos to ensure edge is available
-opam pin add -yn sihl $repo_sihl
+opam pin add -yn sihl https://github.com/leostera/sihl.git#ef4c89fd3d72fdf51caf30082cc55b7e7430b8f2
 opam pin add -yn sihl-cache $repo_sihl
 opam pin add -yn sihl-email $repo_sihl
 opam pin add -yn sihl-queue $repo_sihl
