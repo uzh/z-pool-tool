da178f4111afd8077d3d84b0f353b3febf3c1c5e
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2024-01-31 12:37:41 +0100

    remove comment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,7 +25,6 @@ opam pin add -yn letters $repo_oxi/letters.git#0.3.3
 # pin custom libraries
 opam pin add -yn canary $repo_uzh/canary.git#02cf40e029268560e160ca032850426e387aa598
 opam pin add -yn guardian $repo_uzh/guardian.git#b17e373345cb1e09777747248dbb107520bb0c9a
-# opam pin add -yn guardian https://github.com/uzh/guardian.git#b17e373345cb1e09777747248dbb107520bb0c9a
 
 opam pin add -yn pool .
 OPAMSOLVERTIMEOUT=180 opam depext -y pool
