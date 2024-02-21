16a2333ad2bce3faf8ab90d547a7cc93aa9c9578
Timo Huber <timo.huber@econ.uzh.ch>
2024-02-21 16:04:31 +0100

    group message template buttons in dropdown

--- pool/web/view/component/button.ml
+++ pool/web/view/component/button.ml
@@ -1,8 +1,11 @@
 open Tyxml.Html
 
-let add label path =
+let add ?(is_text = false) label path =
   let open Message_template in
+  let classnames =
+    [ "btn"; "primary" ] @ if is_text then [ "is-text" ] else []
+  in
   a
-    ~a:[ a_class [ "btn"; "primary" ]; a_href (Sihl.Web.externalize_path path) ]
+    ~a:[ a_class classnames; a_href (Sihl.Web.externalize_path path) ]
     [ txt (Format.asprintf "Add %s" (Label.to_human label)) ]
 ;;
--- pool/web/view/component/component_button_group.ml
+++ pool/web/view/component/component_button_group.ml
@@ -1,17 +1,22 @@
 open Tyxml.Html
 
-let dropdown ?(classnames = []) ?(orientation = `Right) buttons =
+let dropdown
+  ?(classnames = [])
+  ?(icon = Component_icon.EllipsisVertical)
+  ?(icon_style = [ "small"; "is-text" ])
+  ?(orientation = `Right)
+  buttons
+  =
   let orientation_cls = function
     | `Left -> "left"
     | `Right -> "right"
   in
+  let open Component_icon in
   div
     ~a:[ a_class ("button-list" :: classnames) ]
     [ div
         ~a:[ a_class [ "has-dropdown"; orientation_cls orientation ] ]
-        [ button
-            ~a:[ a_class [ "btn"; "small"; "is-text" ] ]
-            [ Component_icon.(to_html EllipsisVertical) ]
+        [ button ~a:[ a_class ("btn" :: icon_style) ] [ to_html icon ]
         ; ul
             ~a:[ a_class [ "dropdown" ] ]
             (buttons |> CCList.map CCFun.(CCList.return %> li))
