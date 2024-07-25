18158e99308be2aacb8460d92f14bb62bdd44464
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2024-07-25 15:00:31 +0200

    add migrations for currently unhandled pending job queues and already failed ones

--- pool/app/pool_database/migrations/migration_202407171415.ml
+++ pool/app/pool_database/migrations/migration_202407171415.ml
@@ -59,6 +59,46 @@ let drop_migration_state_from_sihl_queue =
     {sql| DELETE FROM core_migration_state WHERE namespace = 'queue' |sql}
 ;;
 
+let move_where_fragment = "status = 'pending' AND tries != max_tries"
+
+let move_still_pending_queue_jobs =
+  Database.Migration.Step.create
+    ~label:"move still pending queue jobs"
+    [%string
+      {sql|
+      INSERT INTO pool_queue_jobs
+        (uuid, name, input, message_template, tries, max_tries, run_at, status,
+        persisted_at, handled_at, last_error, last_error_at, database_label, created_at, updated_at)
+      SELECT
+        uuid, name, input, message_template, tries, max_tries, run_at, status,
+        persisted_at, handled_at, last_error, last_error_at, database_label, created_at, updated_at
+      FROM pool_queue_jobs_history
+      WHERE %{move_where_fragment}
+    |sql}]
+;;
+
+let remove_still_pending_queue_jobs_from_history =
+  Database.Migration.Step.create
+    ~label:"remove still pending queue jobs from history"
+    [%string
+      {sql|
+      DELETE FROM pool_queue_jobs_history
+      WHERE %{move_where_fragment}
+    |sql}]
+;;
+
+let set_failed_for_jobs =
+  Database.Migration.Step.create
+    ~label:"set failed for jobs"
+    {sql|
+      UPDATE pool_queue_jobs_history
+      SET
+        status = 'failed'
+      WHERE status = 'pending'
+        AND tries = max_tries
+    |sql}
+;;
+
 let migration () =
   Database.Migration.(
     empty "202407171415"
@@ -66,5 +106,8 @@ let migration () =
     |> add_step archive_queue_history_mappings
     |> add_step drop_message_history_table
     |> add_step drop_queue_jobs_table
-    |> add_step drop_migration_state_from_sihl_queue)
+    |> add_step drop_migration_state_from_sihl_queue
+    |> add_step move_still_pending_queue_jobs
+    |> add_step remove_still_pending_queue_jobs_from_history
+    |> add_step set_failed_for_jobs)
 ;;
