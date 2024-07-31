95d4d587d50d69ce77c4f0cef904195581d53d2e
Timo Huber <timo.huber@econ.uzh.ch>
2024-07-31 15:45:41 +0200

    fix temp table queries

--- pool/app/filter/repo/repo.ml
+++ pool/app/filter/repo/repo.ml
@@ -262,7 +262,7 @@ module Sql = struct
         query :: template_queries
         |> CCList.fold_left
              (fun acc cur ->
-               acc @ Repo_utils.find_experiments_by_key Key.Participation cur)
+               acc @ Repo_utils.find_experiments_by_key Key.Invitation cur)
              []
         |> (function
          | [] -> Lwt_result.return ()
@@ -310,7 +310,7 @@ module Sql = struct
         query :: template_queries
         |> CCList.fold_left
              (fun acc cur ->
-               acc @ Repo_utils.find_experiments_by_key Key.Participation cur)
+               acc @ Repo_utils.find_experiments_by_key Key.Assignment cur)
              []
         |> (function
          | [] -> Lwt_result.return ()
