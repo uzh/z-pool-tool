ef565d23af821b3d29424ac3d4e60b22daa5b2be
Timo Huber <timo.huber@econ.uzh.ch>
2024-08-16 15:29:44 +0200

    store notifications in ref and send after migrations were executed

--- pool/command/command_migrate.ml
+++ pool/command/command_migrate.ml
@@ -1,6 +1,50 @@
 open Database
 open Utils.Lwt_result.Infix
 
+module Outbox : sig
+  type t = string * Email.Service.Smtp.prepared
+
+  val add : t -> unit
+  val send : unit -> unit Lwt.t
+end = struct
+  type t = string * Email.Service.Smtp.prepared
+
+  let notifications : t list ref = ref []
+  let add notification = notifications := notification :: !notifications
+
+  let send_notifications () =
+    Logs.info (fun m ->
+      m "Sending %i migration notifications" (List.length !notifications));
+    let send
+      ( sender
+      , { Email.Service.Smtp.reply_to; recipients; subject; body; config; _ } )
+      =
+      Letters.create_email ~reply_to ~from:sender ~recipients ~subject ~body ()
+      |> function
+      | Ok message -> Letters.send ~config ~sender ~recipients ~message
+      | Error msg -> raise (Sihl.Contract.Email.Exception msg)
+    in
+    !notifications
+    |> Lwt_list.iter_s (fun notification ->
+      Lwt.catch
+        (fun () -> send notification)
+        (fun exn ->
+          let err = Printexc.to_string exn in
+          let error_message =
+            Format.asprintf
+              "Error while  notifying about failed migration '%s'"
+              err
+          in
+          Logs.err (fun m -> m "%s" error_message) |> Lwt.return))
+  ;;
+
+  let send () =
+    if Sihl.Configuration.is_development ()
+    then Lwt.return_unit
+    else send_notifications ()
+  ;;
+end
+
 let root =
   Command_utils.make_no_args "migrate.root" "Migrate root database" (fun () ->
     let (_ : status) = Root.add () in
@@ -13,70 +57,46 @@ let root =
     Lwt.return_some ())
 ;;
 
-let notify_about_failure db_label error =
-  let send () =
-    let open Sihl_email in
-    let read_config = Sihl.Configuration.read_string in
-    let sender = read_config "SMTP_SENDER" in
-    let recipient = read_config "SMTP_SENDER" in
-    match sender, recipient with
-    | Some sender, Some recipient ->
-      let text =
-        Format.asprintf
-          {|Migrations on the tenant %s could not be executed successfully.
+let create_failure_notification db_label error =
+  let open Sihl_email in
+  let read_config = Sihl.Configuration.read_string in
+  let sender = read_config "SMTP_SENDER" in
+  let recipient = read_config "SMTP_SENDER" in
+  match sender, recipient with
+  | Some sender, Some recipient ->
+    let text =
+      Format.asprintf
+        {|Migrations on the tenant %s could not be executed successfully.
           
 Error: %s
 
 Please take the necessary actions.|}
-          (Database.Label.value db_label)
-          error
-      in
-      let email =
-        { sender
-        ; recipient
-        ; subject = "Tenant migration failed"
-        ; text
-        ; html = None
-        ; cc = []
-        ; bcc = []
-        }
-      in
-      let%lwt { Email.Service.Smtp.sender
-              ; reply_to
-              ; recipients
-              ; subject
-              ; body
-              ; config
-              }
-        =
-        Email.Service.Smtp.prepare Database.root email
-      in
-      Letters.create_email ~reply_to ~from:sender ~recipients ~subject ~body ()
-      |> (function
-       | Ok message -> Letters.send ~config ~sender ~recipients ~message
-       | Error msg -> raise (Sihl.Contract.Email.Exception msg))
-    | _, _ -> Lwt.return_unit
-  in
-  Lwt.catch
-    (fun () ->
-      if Sihl.Configuration.is_development ()
-      then Logs.err (fun m -> m "%s" error) |> Lwt.return
-      else send ())
-    (fun exn ->
-      let err = Printexc.to_string exn in
-      let error_message =
-        Format.asprintf "Error while notifying about failed migration '%s'" err
-      in
-      Logs.err (fun m -> m "%s" error_message) |> Lwt.return)
+        (Database.Label.value db_label)
+        error
+    in
+    let email =
+      { sender
+      ; recipient
+      ; subject = "Tenant migration failed"
+      ; text
+      ; html = None
+      ; cc = []
+      ; bcc = []
+      }
+    in
+    let%lwt prepared = Email.Service.Smtp.prepare Database.root email in
+    Outbox.add (sender, prepared) |> Lwt.return
+  | _ -> Lwt.return_unit
 ;;
 
 let migrate_tenants db_labels =
+  let open Utils.Lwt_result.Infix in
   let run db_label =
     let set_status = Database.Tenant.update_status db_label in
     let handle_error err =
       let err = Pool_common.(Utils.error_to_string Language.En err) in
       let%lwt () = set_status Status.MigrationsFailed in
-      notify_about_failure db_label err
+      create_failure_notification db_label err
     in
     Lwt.catch
       (fun () ->
@@ -90,7 +110,7 @@ let migrate_tenants db_labels =
         let err = Pool_message.Error.MigrationFailed exn in
         handle_error err)
   in
-  db_labels |> Lwt_list.iter_s run
+  db_labels |> Lwt_list.iter_s run >|> Outbox.send
 ;;
 
 let tenants =
