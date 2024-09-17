0775081edece4842b7a3ffcded949e0b2760b139
Timo Huber <timo.huber@econ.uzh.ch>
2024-09-17 13:32:31 +0200

    fix typo

--- pool/app/organisational_unit/organisational_unit.ml
+++ pool/app/organisational_unit/organisational_unit.ml
@@ -1,7 +1,7 @@
 include Entity
 include Event
 module Guard = Entity_guard
-module VersionHistory = Changelog.T (Entity)
+module VersionHistory = Version_history
 
 let find = Repo.find
 let all = Repo.all
