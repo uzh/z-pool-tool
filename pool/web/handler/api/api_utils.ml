40c8dccc21b965d91bc0eae7423d54566bc726d0
Timo Huber <timo.huber@econ.uzh.ch>
2024-10-03 16:04:16 +0200

    do not throw exception in response handler

--- pool/web/handler/api/api_utils.ml
+++ pool/web/handler/api/api_utils.ml
@@ -1,5 +1,10 @@
 open Utils.Lwt_result.Infix
 
+let json_error ?(language = Pool_common.Language.En) error =
+  let error = Pool_common.Utils.error_to_string language error in
+  `Assoc [ "error", `String error ]
+;;
+
 let respond req result =
   let context = Pool_context.find req in
   (* log errors *)
@@ -9,11 +14,7 @@ let respond req result =
     result context
     ||> (function
            | Ok result -> result
-           | Error error ->
-             let error =
-               Pool_common.Utils.error_to_string Pool_common.Language.En error
-             in
-             `Assoc [ "error", `String error ])
+           | Error error -> json_error error)
     ||> Sihl.Web.Response.of_json
-  | Error (_ : Pool_message.Error.t) -> failwith "Error"
+  | Error error -> json_error error |> Sihl.Web.Response.of_json |> Lwt.return
 ;;
