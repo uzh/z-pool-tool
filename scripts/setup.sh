7e9dc0fa75c8ff2ca442944c6068dfff733e1130
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2025-02-24 14:12:14 +0100

    update pin

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,7 @@ opam remote add default https://opam.ocaml.org
 repo_oxi=https://github.com/oxidizing
 repo_uzh=https://github.com/uzh
 repo_sihl=https://github.com/mabiede/sihl.git#maintenance-2501
+repo_opium=https://github.com/rgrinberg/opium.git#73b16f0487497e02750c1123ead377a56be3be43
 
 # pin Sihl repos to ensure edge is available
 opam pin add -yn sihl $repo_sihl
@@ -17,10 +18,8 @@ opam pin add -yn sihl-email $repo_sihl
 opam pin add -yn sihl-storage $repo_sihl
 opam pin add -yn conformist $repo_oxi/conformist.git#aa7b95d1f39215cdaab8cf96d765d63e41d5f8a6
 opam pin add -yn letters $repo_oxi/letters.git#0.4.0
-
-# TODO: remove pins when the packages are released
-opam pin add -yn opium https://github.com/mabiede/opium.git#upgrade-packages
-opam pin add -yn rock https://github.com/mabiede/opium.git#upgrade-packages
+opam pin add -yn opium $repo_opium
+opam pin add -yn rock $repo_opium
 
 # pin custom libraries
 opam pin add -yn guardian $repo_uzh/guardian.git#287bab9baf9b346efe47999e8b7f456bfefe8c13
