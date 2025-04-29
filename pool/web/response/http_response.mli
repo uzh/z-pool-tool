daa47f3e9125c0b6dd0004fcf85b21b7fd53818a
Timo Huber <timo.huber@econ.uzh.ch>
2025-04-29 14:50:47 +0200

    add function signatures to interface file

--- pool/web/response/http_response.mli
+++ pool/web/response/http_response.mli
@@ -1,13 +1,23 @@
 type url_encoded = (string * string list) list
 
+(** Represents various types of HTTP errors that can occur during request handling. *)
 type http_error =
   | AccessDenied
+  (** The request was denied due to insufficient permissions. Responds with a error note and a 401 status code
+  *)
   | BadRequest of
       (Rock.Request.t -> Rock.Response.t Lwt.t)
       * url_encoded option
       * Pool_message.Error.t
+  (** The request was invalid, optionally including URL-encoded data and an error message.
+      The passed request handler will be called and the error will be shown as a notification.
+      Intended to be used with POST requests to rerender the submitted form *)
   | NotFound of Pool_message.Error.t
+  (** The requested resource was not found. Responds with an error note and a 404 status code.
+  *)
   | RenderError of Pool_message.Error.t
+  (** Renders the occured error with a 400 status code. Intendet to be used to handle errors of GET requests
+  *)
 
 val access_denied : http_error
 val not_found : Pool_message.Error.t -> http_error
@@ -22,6 +32,9 @@ val bad_request
   -> Pool_message.Error.t
   -> http_error
 
+(** [bad_request_on_error ?urlencoded fallback handler] runs [handler]. If an error occurs, [fallback] will be called.
+    [urlencoded] can be passed to fill the current form values. Intended to be used with POST requests.
+*)
 val bad_request_on_error
   :  ?urlencoded:url_encoded
   -> (Rock.Request.t -> Rock.Response.t Lwt.t)
@@ -30,6 +43,8 @@ val bad_request_on_error
 
 val render_error : Pool_message.Error.t -> http_error
 
+(** [render_error_on_error handler] runs [handler] and responds with a note that shows the error and a 400 status code if an error occurs.
+    Intended to be used with GET requests. *)
 val render_error_on_error
   :  ('a, Pool_message.Error.t) Lwt_result.t
   -> ('a, http_error) Lwt_result.t
