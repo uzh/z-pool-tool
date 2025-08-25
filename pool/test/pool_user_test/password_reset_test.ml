508ec329e23fc3aee3cafe1c4c76c38125bbf7ed
Yannick Koller <yannick.koller@econ.uzh.ch>
2025-08-25 15:24:58 +0200

    test: add reset password tests
    - test password with policy violation
    - test password cqrs command to event translation

--- pool/test/pool_user_test/password_reset_test.ml
+++ pool/test/pool_user_test/password_reset_test.ml
@@ -40,7 +40,101 @@ let reset_password_succeeds =
     Lwt.return_ok ())
 ;;
 
+let reset_password_fail_password_policy =
+  Test_utils.case (fun () ->
+    let open Pool_user in
+    let open Utils.Lwt_result.Infix in
+    let email =
+      EmailAddress.of_string "reset_password_fail_password_policy@example.com"
+    in
+    let new_password = "password1!" in
+    let%lwt (_ : t) =
+      create_user
+        database_label
+        email
+        (Lastname.of_string "Star")
+        (Firstname.of_string "Jane")
+        created_password
+        created_password_confirmation
+      ||> Pool_common.Utils.get_or_failwith
+    in
+    let%lwt token =
+      Pool_user.Password.Reset.create_token database_label email
+      ||> CCOption.get_exn_or "User with email not found"
+    in
+    let%lwt result =
+      let open Cqrs_command.User_command.ResetPassword in
+      let command_data =
+        [ "token", [ token ]
+        ; "password", [ new_password ]
+        ; "password_confirmation", [ new_password ]
+        ]
+      in
+      command_data |> decode |> Lwt_result.lift >== handle
+    in
+    Alcotest.(check (result (list Test_utils.event) Test_utils.error))
+      "Expected validation error not found"
+      (Error
+         Pool_message.(
+           Error.conformist [ Field.Password, Error.PasswordPolicyCapitalLetter ]))
+      result;
+    Lwt.return_ok ())
+;;
+
+let reset_password_events =
+  Test_utils.case (fun () ->
+    let open Pool_user in
+    let open Utils.Lwt_result.Infix in
+    let email = EmailAddress.of_string "reset_password_with_notification@example.com" in
+    let new_password = "Password1!" in
+    let%lwt _ =
+      create_user
+        database_label
+        email
+        (Lastname.of_string "Star")
+        (Firstname.of_string "Jane")
+        created_password
+        created_password_confirmation
+      ||> Pool_common.Utils.get_or_failwith
+    in
+    let%lwt token =
+      Pool_user.Password.Reset.create_token database_label email
+      ||> CCOption.get_exn_or "User with email not found"
+    in
+    let* events =
+      let open Cqrs_command.User_command.ResetPassword in
+      let command_data =
+        [ "token", [ token ]
+        ; "password", [ new_password ]
+        ; "password_confirmation", [ new_password ]
+        ]
+      in
+      command_data |> decode |> Lwt_result.lift >== handle
+    in
+    let expected_events =
+      [ Pool_user.PasswordReset
+          ( token
+          , Password.Plain.create new_password
+          , Password.Confirmation.create new_password )
+        |> Pool_event.user
+      ]
+    in
+    Alcotest.(check (list Test_utils.event))
+      "Expected events do not match"
+      expected_events
+      events;
+    Lwt.return_ok ())
+;;
+
 let suite =
   Alcotest_lwt.
-    [ "password reset", [ test_case "password reset" `Quick reset_password_succeeds ] ]
+    [ ( "password reset"
+      , [ test_case "password reset" `Quick reset_password_succeeds
+        ; test_case
+            "password reset fail password policy"
+            `Quick
+            reset_password_fail_password_policy
+        ; test_case "password reset events" `Quick reset_password_events
+        ] )
+    ]
 ;;
