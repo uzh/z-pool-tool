2c4aa838b0a08228c5618228cf5f2bf00436bc83
Yannick Koller <yannick.koller@econ.uzh.ch>
2025-11-11 11:06:49 +0100

    fix racecondition during password reset

--- pool/app/pool_user/event.ml
+++ pool/app/pool_user/event.ml
@@ -38,10 +38,10 @@ let handle_event ?tags pool : event -> unit Lwt.t =
     in
     Lwt.return_unit
   | PasswordReset (token, new_password, confirmation) ->
-    let%lwt (_ : (unit, Pool_message.Error.t) result) =
+    let%lwt _ =
       PasswordReset.reset_password ~token pool new_password confirmation
-      >> (Pool_token.deactivate pool token |> Lwt_result.ok)
       >|- Pool_common.Utils.with_log_error ~src ?tags
+      >|> fun _ -> Pool_token.deactivate pool token
     in
     Lwt.return_unit
 ;;
