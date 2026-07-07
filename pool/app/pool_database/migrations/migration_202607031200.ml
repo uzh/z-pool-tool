dc56d7dd593a26908ca8c97bd246d45fef7cd349
Yannick Koller <yannick.koller@econ.uzh.ch>
2026-07-07 17:05:44 +0200

    chore: improve pool_authentication token validity migration

--- pool/app/pool_database/migrations/migration_202607031200.ml
+++ pool/app/pool_database/migrations/migration_202607031200.ml
@@ -1,14 +1,25 @@
+let remove_automagic_timestamp_on_update_trigger =
+  Database.Migration.Step.create
+    ~label:
+      "remove mariadbs automagic timestamp on update trigger for token valid_until column"
+    {sql|
+      ALTER TABLE pool_authentication
+      MODIFY COLUMN valid_until TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()
+    |sql}
+;;
+
 let add_usage_count_to_authentication =
   Database.Migration.Step.create
     ~label:"add usage_count to pool_authentication"
     {sql|
       ALTER TABLE pool_authentication
-        MODIFY COLUMN valid_until TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-        ADD COLUMN usage_count TINYINT UNSIGNED NOT NULL AFTER token
+      ADD COLUMN usage_count TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER token
     |sql}
 ;;
 
-(* This migration is executed on root and tenant databases *)
 let migration () =
-  Database.Migration.(empty "202607031200" |> add_step add_usage_count_to_authentication)
+  Database.Migration.(
+    empty "202607031200"
+    |> add_step remove_automagic_timestamp_on_update_trigger
+    |> add_step add_usage_count_to_authentication)
 ;;
